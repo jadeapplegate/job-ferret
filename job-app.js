78a0e6875896c7f4802d0444a99796859492796c
Jade <jade.applegate@gmail.com>
2014-06-18 20:25:31 -0700

    added bootstrap, github oauth, and accounts-ui packages

--- job-app.js
+++ job-app.js
@@ -1,19 +1,25 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to job-app.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "Welcome to job-app.";
+  // };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  // Template.hello.events({
+  //   'click input': function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined'){
+  //       alert("You pressed the button");
+  //     }
+  //   }
+  // });
+
+  Template.addJob.addInfo = function () {
+
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    
   });
 }
