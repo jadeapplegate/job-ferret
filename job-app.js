c980e0dbcfbb92d64ae34621121e3c943f51243a
Jade <jade.applegate@gmail.com>
2014-06-18 21:48:31 -0700

    added functionality for adding jobs of interest, with delete and pursue options

--- job-app.js
+++ job-app.js
@@ -1,3 +1,5 @@
+Jobs = new Meteor.Collection("jobs");
+
 if (Meteor.isClient) {
   // Template.hello.greeting = function () {
   //   return "Welcome to job-app.";
@@ -12,14 +14,44 @@ if (Meteor.isClient) {
   //   }
   // });
 
-  Template.addJob.addInfo = function () {
-
+  Template.jobsList.jobs = function () {
+    return Jobs.find();
   };
+ 
+  Template.jobsList.events({
+    'click .delete' : function(event, template) {
+      event.preventDefault();
+      var jobId = $(event.target).attr('data-id');
+      Jobs.remove({_id: jobId});
+    },
+    'click .pursue' : function(event, template) {
+      event.preventDefault();
+      var jobId = $(event.target).attr('data-id');
+      Jobs.update({_id: jobId}, {$set: {pursuing: true}})
+      console.log(Jobs.findOne({}))
+    }
+  });
+
+  Template.addJob.events({
+    'click #addJob' : function(event, template) {
+      event.preventDefault();
+      var data = {
+        link: template.find("input[name=link]").value,
+        title: template.find("input[name=title]").value,
+        company: template.find("input[name=company]").value,
+        pursuing: false
+      };
+
+      Jobs.insert(data);
+    }
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
     // code to run on server at startup
-    
+
   });
 }
+  
