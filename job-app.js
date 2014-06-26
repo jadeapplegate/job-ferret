76bd0dfa30782afa93183b7d39ec20df8c0569ba
Jade <jade.applegate@gmail.com>
2014-06-26 01:46:39 -0700

    form input styling

--- job-app.js
+++ job-app.js
@@ -1,5 +1,5 @@
 // Jobs = new Meteor.Collection("jobs");
 
-// Jobs.remove({});
-// Jobs.insert({title: 'engineer', company: 'twitter', link: 'http://twitter.com/jobs', pursuing:true, min_salary: "$80,000"});
-// Jobs.insert({title: 'engineer', company: 'box', link: 'http://twitter.com/jobs', pursuing:false, min_salary: "$80,000"});
+Jobs.remove({});
+// Jobs.insert({title: 'Engineer', company: 'Twitter', link: 'http://twitter.com/jobs', pursuing:false, min_salary: "$80,000"});
+// Jobs.insert({title: 'Developer', company: 'Box', link: 'http://twitter.com/jobs', pursuing:false, min_salary: "$80,000"});
